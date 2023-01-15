dff1c0948f3ffb70a7b7271a39e58b75f611fbe2
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2023-01-16 10:52:09 +1300

    Ruby 3.2.0 'official' support

--- lib/ooxml_encryption/version.rb
+++ lib/ooxml_encryption/version.rb
@@ -5,11 +5,11 @@ class OoxmlEncryption
   # Gem version. If this changes, be sure to re-run "bundle install" or
   # "bundle update".
   #
-  VERSION = '0.1.0'
+  VERSION = '0.2.0'
 
   # Date for VERSION. If this changes, be sure to re-run "bundle install"
   # or "bundle update".
   #
-  DATE = '2022-08-08'
+  DATE = '2023-01-16'
 
 end
